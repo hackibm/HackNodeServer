14417e4eb951161126134cef112c9383713f4fed
alipska <alipska.rybicka@gmail.com>
2016-11-28 13:26:39 +0100

    compliance with eslint

--- __tests__/DistrictOfficeContact-test.js
+++ __tests__/DistrictOfficeContact-test.js
@@ -1,23 +1,20 @@
-"use strict";
+import jest from 'jest';
 
 jest.dontMock('../components/DistrictOfficeContact.jsx');
 
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
 import DistrictOfficeContact from '../components/DistrictOfficeContact.jsx';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 
-describe('DistrictOfficeContact', function() {
+describe('DistrictOfficeContact', () => {
+  describe('The DistrictOfficeContact Display', () => {
+    const contact = shallow(<DistrictOfficeContact districtName="Wola"/>);
+    it('should be a div', () => {
+      expect(contact.type()).toBe('div');
+    });
 
-	  describe("The DistrictOfficeContact Display",()=>{
-			const contact = shallow(<DistrictOfficeContact districtName='Wola'/>);
-
-			it("should be a div",()=>{	
-				expect(contact.type()).toBe('div');			
-			});
-
-			it("should display suitable text",()=>{
-				 expect(contact.find('div').text()).toEqual("Dane kontaktowe dla: Wola");
-			});
-	})
-})
+    it('should display suitable text', () => {
+      expect(contact.find('div').text()).toEqual('Dane kontaktowe dla: Wola');
+    });
+  });
+});
